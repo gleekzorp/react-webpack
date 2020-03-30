7f56eadcef5193db9023c9e9b405b2f2655ef6e3
Daniel Floyd <gleek.dan@gmail.com>
2020-03-29 19:54:30 -0600

    Initial commit.  React works with a basic example.  I have a todo list of things to test

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import './index.css';;
+
+const App = () => {
+  return (
+    <div className="app">
+        <h1>It works!</h1>
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
