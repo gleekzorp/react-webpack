e844fcad9e3feba2359aa37fddcd35c63dae9ea9
Daniel Floyd <gleek.dan@gmail.com>
2020-03-29 21:23:58 -0600

    react-router-dom works in development but I can't figure out production build

--- src/index.js
+++ src/index.js
@@ -1,20 +1,32 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+
+// import './index.css';
+// import Home from './components/Home';
+// import About from './components/About';
+// import Contact from './components/Contact';
+
+// const App = () => {
+//   return (
+//     <div className="app">
+//         <h1>It works!</h1>
+//         <Home />
+//         <About name="Daniel"/>
+//         <Contact />
+//     </div>
+//   );
+// };
+
+// ReactDOM.render(<App />, document.getElementById('root'));
+
+
+
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import './index.css';
-import Home from './components/Home';
-import About from './components/About';
-import Contact from './components/Contact';
+import App from './App';
 
-const App = () => {
-  return (
-    <div className="app">
-        <h1>It works!</h1>
-        <Home />
-        <About name="Daniel"/>
-        <Contact />
-    </div>
-  );
-};
+import './index.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
